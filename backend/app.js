611585b599a1d5cb59156a5c94b95158b588e9a7
msrdnv <msrdnv@ya.ru>
2023-09-27 18:18:54 +0300

    cors update

--- backend/app.js
+++ backend/app.js
@@ -31,11 +31,10 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', '*');
     return res.end();
   }
   return next();

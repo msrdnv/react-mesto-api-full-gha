3dbcda2c4a58416686af9f35b26d0dc9ce4c3c0a
msrdnv <msrdnv@ya.ru>
2023-09-27 18:57:43 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,6 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    return res.end();
   }
   return next();
 });
@@ -35,8 +34,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', 'Content-type');
-    return res.end();
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   }
   return next();
 });

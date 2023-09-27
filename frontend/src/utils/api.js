854505aa7cf5b28f1b690dfeb017ae6947e0e876
msrdnv <msrdnv@ya.ru>
2023-09-27 17:39:11 +0300

    https add

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -50,7 +50,7 @@ class Api {
 };
 
 export const api = new Api({
-  baseUrl: 'http://mesto-backend.msrdnv.nomoredomainsrocks.ru',
+  baseUrl: 'https://mesto-backend.msrdnv.nomoredomainsrocks.ru',
   headers: {
     'Authorization' : `Bearer ${localStorage.getItem('token')}`,
     'Content-Type': 'application/json'
